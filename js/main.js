8e6305416f8ee70c3cb977e7607a94a44d9c8dc3
Acter4ik <ferum.conarev@gmail.com>
2026-06-14 13:57:34 +0300

    add new section

--- js/main.js
+++ js/main.js
@@ -1,6 +1,20 @@
-Fancybox.bind("[data-fancybox]", {
-  // width: "90%", 
-  // maxWidth: 900,
-  // height: "70%", 
-  // maxHeight: 600
+$(function () {
+  // --------------FancyBox--------------
+  Fancybox.bind("[data-fancybox]", {
+    // width: "90%", 
+    // maxWidth: 900,
+    // height: "70%", 
+    // maxHeight: 600
+  });
+
+  // -----------------MixItUp---------------
+  var mixer = mixitup('.directions__list');
+
+  // --------------------AddPrefixForDirectionsButtons---------------
+  $('.directions__filter-btn').on('click', function () {
+    $('.directions__filter-btn').removeClass('directions__filter-btn--active')
+    $(this).addClass('directions__filter-btn--active')
+  })
+
+
 });
