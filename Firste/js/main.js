340ab10491e815839044c72dce6cb4ffadba6e2f
Acter4ik <ferum.conarev@gmail.com>
2026-06-17 22:15:19 +0300

    fix for testimonial section

---
+++ Firste/js/main.js
@@ -0,0 +1,6 @@
+Fancybox.bind("[data-fancybox]", {
+  // width: "90%", 
+  // maxWidth: 900,
+  // height: "70%", 
+  // maxHeight: 600
+});
--- js/main.js
+++ js/main.js
@@ -32,6 +32,24 @@ $(function () {
     $('.team__slider').slick('slickNext')
   })
 
+  $('.testimonials__slider').slick({
+    arrows:false,
+    slidesToShow: 1,
+    draggable: false,
+    dots: true,
+    appendDots: $('.testimonials__dots'),
+  })
+
+  $('.testimonials__slider-prev').on('click', function(e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickPrev')
+  })
+
+  $('.testimonials__slider-next').on('click', function(e) {
+    e.preventDefault()
+    $('.testimonials__slider').slick('slickNext')
+  })
+
 
 
 
